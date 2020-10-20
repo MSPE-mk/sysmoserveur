63863fd0a6d9b67e7dd29a98232a789c890c4493
swis2020 <swis.contact@topnet.tn>
2020-10-20 11:33:30 +0100

    ok

--- src/models/produit.model.js
+++ src/models/produit.model.js
@@ -3,19 +3,19 @@ var dbConn = require('./../../config/db.config');
 //produits object create
 // display add produit page  (`nom`,`reference`,`categorie`,`prix`,`disponibilite`,`description`
 
-var Produit = function (produit) {
+var Produit = (produit) => {
   this.nom = produit.nom;
   this.reference = produit.reference;
   this.categorie = produit.categorie;
   this.prix = produit.prix;
   this.disponibilite = produit.disponibilite;
   this.description = produit.description;
-
   this.created_at = new Date();
   this.updated_at = new Date();
 };
-Produit.create = function (newProd, result) {
-  dbConn.query("INSERT INTO produits set ?", newProd, function (err, res) {
+
+Produit.create = (newProd, result) => {
+  dbConn.query("INSERT INTO produits set ?", newProd, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -26,8 +26,9 @@ Produit.create = function (newProd, result) {
     }
   });
 };
-Produit.findById = function (id, result) {
-  dbConn.query("Select * from produits where id = ? ", id, function (err, res) {
+
+Produit.findById = (id, result) => {
+  dbConn.query("Select * from produits where id = ? ", id, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -38,20 +39,21 @@ Produit.findById = function (id, result) {
   });
 };
 
-Produit.findByCategorie = function (id, result) {
-  dbConn.query("Select * from produits where categorie = ? ", id, function (err, res) {
+Produit.findByCategorie = (id, result) => {
+  dbConn.query("Select * from produits where categorie = ? ", id, (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
     }
     else {
       result(null, res);
-      console.log('Find product by categorie'+ res);
+      console.log('Find product by categorie' + res);
     }
   });
 };
-Produit.findAll = function (result) {
-  dbConn.query("Select * from produits", function (err, res) {
+
+Produit.findAll = (result) => {
+  dbConn.query("Select * from produits", (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);
@@ -64,7 +66,7 @@ Produit.findAll = function (result) {
 };
 // display add produit page  (`nom`,`reference`,`categorie`,`prix`,`disponibilite`,`description`
 
-Produit.update = function (id, produit, result) {
+Produit.update = (id, produit, result) => {
   dbConn.query("UPDATE produits SET nom=?,reference=?,categorie=?,prix=?,disponibilite=?,description=? WHERE id = ?", [produit.nom, produit.reference, produit.categorie, produit.prix, produit.disponibilite, produit.description, id], function (err, res) {
     if (err) {
       console.log("error: ", err);
@@ -74,8 +76,9 @@ Produit.update = function (id, produit, result) {
     }
   });
 };
-Produit.delete = function (id, result) {
-  dbConn.query("DELETE FROM produits WHERE id = ?", [id], function (err, res) {
+
+Produit.delete = (id, result) => {
+  dbConn.query("DELETE FROM produits WHERE id = ?", [id], (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);
