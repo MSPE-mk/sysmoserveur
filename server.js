a0fc9d267e8d1f9dc31993e573e9ed96e9a978f1
swis2020 <swis.contact@topnet.tn>
2020-10-21 15:00:26 +0100

    add upload service

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
 // create express app
 const app = express();
+// use Upload Module
+app.use(fileUpload());
 // Access-Control-Allow-Origin
 app.use(cors());
 // Setup server port
@@ -16,6 +19,24 @@ app.use(bodyParser.json())
 // define a root route
 app.get('/', (req, res) => {
   res.send("Hello World");
+});
+
+app.post('/upload', function(req, res) {
+  console.log(req.files.uploadedImg.name);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.uploadedImg;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(__dirname+'/uploads/climatiseurs/'+req.files.uploadedImg.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 // Require produit routes
 const produitRoutes = require('./src/routes/produit.routes')
